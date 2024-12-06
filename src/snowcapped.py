069ba2cc87bf7d659e5a8ae69c8baca743bf56f6
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-12-06 04:21:25 +0100

    upper layer, caves, ghost forest and more

--- src/snowcapped.py
+++ src/snowcapped.py
@@ -10,7 +10,7 @@ def beet_default(ctx: Context):
     dimension_file = cache / "dimension.json"
     biome_colors_file = cache / "biome_colors.json"
 
-    check_call(['npx', 'snowcapped', 'snowcapped.json', '-d', dimension_file, " -c ", biome_colors_file], stdout=DEVNULL, stderr=STDOUT)
+    check_call(['npx', 'snowcapped', 'snowcapped.json', '-d', dimension_file, "-c", biome_colors_file], stdout=DEVNULL, stderr=STDOUT)
 
     ctx.data["minecraft:the_nether"] = Dimension(source_path=dimension_file)
     ctx.data["c:biome_colors"] = BiomeColors(source_path=biome_colors_file)
