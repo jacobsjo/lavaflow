16bff25fc1d53ff34ccfd9efa1d3912685459021
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-04-26 00:22:46 +0200

    don't error when GITHUB_OUTPUT is not set

--- plugins/githubOutput.py
+++ plugins/githubOutput.py
@@ -2,5 +2,6 @@ import os
 from beet import Context
 
 def beet_default(ctx: Context):
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f'version={ctx.project_version}', file=fh)
+    if 'GITHUB_OUTPUT' in os.environ:
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+            print(f'version={ctx.project_version}', file=fh)
